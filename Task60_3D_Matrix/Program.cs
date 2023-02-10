a9aa56f05d1e3745b2ba2750715edb8d1ae6b839
Dmitry <dimakameya@gmail.com>
2023-02-10 23:56:47 +0500

    исправил код в задаче 60

--- Task60_3D_Matrix/Program.cs
+++ Task60_3D_Matrix/Program.cs
@@ -49,20 +49,57 @@ for (int i = 0; i < matrix.GetLength(0); i++)
 }
 */
 
-Dictionary<int, int> UniqueNumbers = new Dictionary<int, int>();
-Random rnd = new Random();
-int number = rnd.Next(10, 100);
-while(UniqueNumbers.Count < 8)
+int [] ArrayUniqueNumber(int countElement)
 {
-    if(UniqueNumbers.ContainsKey(number))
-        number = rnd.Next(10, 100);
-    else
-        UniqueNumbers.Add(number, 1);
+    int [] array = new int [countElement];
+    Dictionary<int, int> UniqueNumbers = new Dictionary<int, int>();
+    Random rnd = new Random();
+    int number = rnd.Next(10, 100);
+//    int countElement = rows * columns * layers;
+    while(UniqueNumbers.Count < countElement)
+    {
+        if(UniqueNumbers.ContainsKey(number))
+            number = rnd.Next(10, 100);
+        else
+            UniqueNumbers.Add(number, 1);
+    }
+    int i = 0;
+    foreach (var item in UniqueNumbers)
+    {
+        array[i] = item.Key;
+        Console.Write($"{array[i]} ");
+        i++;
+    }
+    return array;
 }
 
-
-foreach (var item in UniqueNumbers)
+int[,,] Init3DArrayUniqueElement(int rows, int columns, int layers)
 {
-    Console.WriteLine($"{item.Key} встречается {item.Value} раз");
+    int[,,] array3D = new int[rows, columns, layers];
+    int countElement = rows * columns * layers;
+    int [] arrayUnique = ArrayUniqueNumber(countElement);
+    Console.WriteLine();
+    int n = 0;
+
+    for (int i = 0; i < array3D.GetLength(0); i++)
+    {
+        for (int j = 0; j < array3D.GetLength(1); j++)
+        {
+            for (int m = 0; m < array3D.GetLength(2); m++)
+            {
+                
+                array3D[i, j, m] = arrayUnique[n];
+                Console.Write($"{array3D[i,j,m]}({i},{j},{m}) ");
+                n++;
+            }            
+            Console.WriteLine();
+        }
+    }
+    
+    return array3D;
 }
 
+int [,,] array3D = Init3DArrayUniqueElement(2, 2, 2);
+
+
+//int [] array = ArrayUniqueNumber(8);
